c2f28fa138fe618efade3ab23f4f31dc5814dcca
“Shivam <shivam6132@gmail.com>
2025-08-23 07:08:06 -0700

    Customize UserButton appearance in Header component

--- components/header.jsx
+++ components/header.jsx
@@ -45,7 +45,14 @@ const Header = () => {
               </SignInButton>
         </SignedOut>
         <SignedIn>
-                <UserButton />
+                <UserButton 
+                appearance={{
+                  elements: {
+                   avatarBox: "w-10 h-10",
+                    
+                  }
+                }} 
+                />
         </SignedIn>
         </div>
           </nav>
