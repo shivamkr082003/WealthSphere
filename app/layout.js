3a2e06e8a6d32d7a45054921638047f4cba8f60c
“Shivam <shivam6132@gmail.com>
2025-08-23 21:10:53 -0700

    feat: implement account management features with CRUD operations
    - Added account creation and update functionalities with validation using Zod.
    - Implemented transaction seeding for testing purposes.
    - Created API routes for account and transaction management.
    - Integrated user authentication with Clerk and Prisma for database operations.
    - Developed UI components for account display and transaction tables.
    - Enhanced dashboard layout and account card components for better user experience.
    - Added error handling and loading states for asynchronous operations.
    - Introduced ArcJet for rate limiting and bot detection.

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 
 
@@ -29,6 +30,8 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen">
           {children}
         </main>
+
+        <Toaster richColors />
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto text-center px-4 text-gray-600">
             <p className="text-gray-600">© 2023 Welth. All rights reserved.</p>
