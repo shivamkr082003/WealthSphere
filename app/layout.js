dc965ca6195b48170588500c896bd3af4902c718
“Shivam <shivam6132@gmail.com>
2026-02-28 10:08:33 -0800

    Refactor code structure for improved readability and maintainability

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "./components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import ErrorBoundary from "@/components/custom-error-boundary";
 
