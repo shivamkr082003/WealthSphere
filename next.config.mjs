a5782b870044b7f20cb208ef81d8d378138747bd
“Shivam <shivam6132@gmail.com>
2026-03-18 17:54:47 -0700

    fix: reorganize next.config.mjs for improved structure and clarity

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,26 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
+    turbopack: {
+        root: __dirname,
+    },
+    images: {
+        remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'randomuser.me',
+                protocol: "https",
+                hostname: "randomuser.me",
             },
         ],
     },
     experimental: {
-       serverActions:{
-        bodySizeLimit: '5mb',
-       }
+        serverActions: {
+            bodySizeLimit: "5mb",
+        },
     },
 };
 
